b935003edb2482b90a70d0454378e70373881441
DestinyMatuse <lillymatuse@gmail.com>
2023-10-18 06:54:52 -0700

    update

--- 0x18-dynamic_libraries/0-main.c
+++ 0x18-dynamic_libraries/0-main.c
@@ -1,32 +1,13 @@
 #include "main.h"
 #include <stdio.h>
-#include <stdlib.h>
 
 /**
- * _strlen - calculate the length of a string
- * @str: the input string
- *
- * Return: the length of the string
- */
-int _strlen(const char *str)
-{
-    int length = 0;
-
-    while (str[length] != '\0')
-    {
-        length++;
-    }
-    return length;
-}
-
-/**
- * main - Entry point
+ * main - check the code
  *
  * Return: Always EXIT_SUCCESS.
  */
 int main(void)
 {
     printf("%d\n", _strlen("My Dyn Lib"));
-    return EXIT_SUCCESS;
+    return (EXIT_SUCCESS);
 }
-
